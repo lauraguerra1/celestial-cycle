5f4b1c1fbd9a5422710a1f104323eb5db363cc74
Sandra Chatphatthanasiri <sakisandra.c@gmail.com>
2023-10-16 19:09:03 -0600

    Add homepage component and background

---
+++ src/components/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+        <h1>Daily Horoscope</h1>
+        <h2>October 12th 2023</h2>
+    </div>
+  )
+}
+
+export default Homepage
