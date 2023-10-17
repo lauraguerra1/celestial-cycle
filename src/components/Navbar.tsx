f44be4c12a285132ffdac5223f094f6315d6fe11
Sandra Chatphatthanasiri <sakisandra.c@gmail.com>
2023-10-17 10:22:26 -0600

    Add links to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,28 +3,36 @@ import home from '../../public/images/house.png'
 import calendar from '../../public/images/calendar.png'
 import user from '../../public/images/user.png'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
+
     return (
         <nav className='flex fixed bottom-0 h-20 w-full bg-lightgray'>
             <div className='flex w-full h-20 icon-container items-center'>
                 <div className='h-5/6 w-20 flex p-5 justify-center flex-col items-center'>
+                    <Link href='/calendar'>
                     <div className='rounded-xl bg-darkgray p-33 icon'>
                         <Image width={45} height={45} alt='user logo' src={calendar} />
                     </div>
                     <p>Calendar</p>
+                    </Link>
                 </div>
-                <div className='h-5/6 w-20 flex p-5 justify-center flex-col items-center'>
+                <div className='h-5/6 w-20 flex p-5 justify-center flex-col items-center'> 
+                    <Link href='/'>
                     <div className='rounded-xl bg-darkgray p-33 icon'>
                         <Image width={45} height={45} alt='user logo' src={home} />
                     </div>
                     <p>Home</p>
+                    </Link>
                 </div>
                 <div className='h-5/6 w-20 flex p-5 justify-center flex-col items-center'>
+                    <Link href='/insights'>
                     <div className='rounded-xl bg-darkgray p-33 icon'>
                         <Image width={45} height={45} alt='user logo' src={user} />
                     </div>
                     <p>Logout</p>
+                    </Link>
                 </div>
             </div>
         </nav>
