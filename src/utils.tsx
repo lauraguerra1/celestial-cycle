1cbf5f69469106fa9bc7839fc3bf0a137bb0b802
Sandra Chatphatthanasiri <sakisandra.c@gmail.com>
2023-10-17 09:46:32 -0600

    Write function to find zodiac sign

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,25 +1,30 @@
 const zodiacSigns = [
-    { sign: "Capricorn", start: "12/22", end: "1/19" },
-    { sign: "Aquarius", start: "1/20", end: "2/18" },
-    { sign: "Pisces", start: "2/19", end: "3/20" },
-    { sign: "Aries", start: "3/21", end: "4/19" },
-    { sign: "Taurus", start: "4/20", end: "5/20" },
-    { sign: "Gemini", start: "5/21", end: "6/20" },
-    { sign: "Cancer", start: "6/21", end: "7/22" },
-    { sign: "Leo", start: "7/23", end: "8/22" },
-    { sign: "Virgo", start: "8/23", end: "9/22" },
-    { sign: "Libra", start: "9/23", end: "10/22" },
-    { sign: "Scorpio", start: "10/23", end: "11/21" },
-    { sign: "Sagittarius", start: "11/22", end: "12/21" }
+    { sign: "Capricorn", start: 1222, end: 119 },
+    { sign: "Aquarius", start: 120, end: 218 },
+    { sign: "Pisces", start: 219, end: 320 },
+    { sign: "Aries", start: 321, end: 419 },
+    { sign: "Taurus", start: 420, end: 520 },
+    { sign: "Gemini", start: 521, end: 620 },
+    { sign: "Cancer", start: 621, end: 722 },
+    { sign: "Leo", start: 723, end: 822 },
+    { sign: "Virgo", start: 823, end: 922 },
+    { sign: "Libra", start: 923, end: 1022 },
+    { sign: "Scorpio", start: 1023, end: 1121 },
+    { sign: "Sagittarius", start: 1122, end: 1221 }
   ];
 
 export const getZodiacSign = (birthday: string) => {
     const month = birthday.split('/')[0]
     const day = birthday.split('/')[1]
+    const date = parseInt(`${month}${day}`)
 
-    zodiacSigns.filter(sign => {
-        return 
-    })
+    const mySign = zodiacSigns.filter(sign => date >= sign.start && date <= sign.end)
+
+    if(mySign.length === 0) {
+        return "capricorn"
+    } else {
+        return mySign[0].sign.toLowerCase()
+    }
 }
 
 export const getTodaysDate = () => {
