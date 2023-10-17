16be70d4df1f72c90ac2758aa043625350f09982
Amber Shipley <ashipley2@me.com>
2023-10-17 09:49:13 -0700

    Allow supabase to verify token from passage for authN

---
+++ src/utils/supabase.ts
@@ -0,0 +1,29 @@
+import { createClient, SupabaseClientOptions } from "@supabase/supabase-js";
+import jwt from "jsonwebtoken";
+
+const getSupabase = (userId: string) => {
+  const options: SupabaseClientOptions<"public"> = {};
+
+  if (userId) {
+    const payload = {
+      userId,
+      exp: Math.floor(Date.now() / 1000) + 60 * 60,
+    };
+    const token = jwt.sign(payload, process.env.SUPABASE_JWT_SECRET as string);
+
+    options.global = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+  }
+
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+    options
+  );
+  return supabase;
+};
+
+export { getSupabase };
