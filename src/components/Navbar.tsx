bf0d869f10a2358ba5754bdc9b4dad013c9303b8
Calli Herrmann <calliherrmann@gmail.com>
2023-10-18 12:05:05 -0400

    Add signout functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,15 @@ import calendar from '../../public/images/calendar.png'
 import user from '../../public/images/user.png'
 import Image from 'next/image'
 import Link from 'next/link'
+import Router from "next/router";
+import { PassageUser } from "@passageidentity/passage-elements/passage-user";
 
 const Navbar = () => {
+
+    const signOut = async () => {
+        new PassageUser().signOut();
+        Router.push("/");
+      };
 
     return (
         <nav className='flex fixed bottom-0 h-20 w-full bg-lightgray'>
@@ -13,7 +20,7 @@ const Navbar = () => {
                 <div className='h-5/6 w-20 flex p-5 justify-center flex-col items-center'>
                     <Link href='/calendar'>
                     <div className='rounded-xl bg-darkgray p-33 icon'>
-                        <Image width={45} height={45} alt='user logo' src={calendar} />
+                        <Image width={45} height={45} alt='calendar logo' src={calendar} />
                     </div>
                     <p>Calendar</p>
                     </Link>
@@ -21,18 +28,16 @@ const Navbar = () => {
                 <div className='h-5/6 w-20 flex p-5 justify-center flex-col items-center'> 
                     <Link href='/'>
                     <div className='rounded-xl bg-darkgray p-33 icon'>
-                        <Image width={45} height={45} alt='user logo' src={home} />
+                        <Image width={45} height={45} alt='home logo' src={home} />
                     </div>
                     <p>Home</p>
                     </Link>
                 </div>
                 <div className='h-5/6 w-20 flex p-5 justify-center flex-col items-center'>
-                    <Link href='/insights'>
-                    <div className='rounded-xl bg-darkgray p-33 icon'>
+                    <div className='rounded-xl bg-darkgray p-33 icon' onClick={signOut}>
                         <Image width={45} height={45} alt='user logo' src={user} />
                     </div>
                     <p>Logout</p>
-                    </Link>
                 </div>
             </div>
         </nav>
