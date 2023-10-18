670b96bff8a8bc0b2de5acf171bbdb0dc539d411
Laura Guerra <l.garciaguerra1@gmail.com>
2023-10-17 20:42:44 -0400

    Update cypress test for demo user

--- cypress/e2e/login_spec.cy.ts
+++ cypress/e2e/login_spec.cy.ts
@@ -2,12 +2,10 @@ describe('template spec', () => {
   it('should take you to authentication', () => {
     cy.visit(`http://localhost:3000`)
     cy.get('h1').contains('Celestial Cycle')
-      .get('p').contains('Click to demo')
-      //.click()
-      //add testing after we have demo account working
-      // .url().should('equal', 'http://localhost:3000/')
-      // .get('button').contains('LOG OUT').click()
-      // .url().should('equal', 'http://localhost:3000/login')
+      .get('p').contains('Click to demo').click()
+      .url().should('contain', 'demo/dashboard')
+      .get('button').contains('Logout').click()
+      .url().should('equal', 'http://localhost:3000/')
       .get('button').contains('Login').click()
       .url().should('contain', 'authenticate')
   })
