d3f14046b9380f11aad3383781212dbbe53c7004
Laura Guerra <l.garciaguerra1@gmail.com>
2023-10-19 11:33:52 -0400

    Add entry endpoint

--- src/pages/api/addEntry.ts
+++ src/pages/api/addEntry.ts
@@ -3,18 +3,40 @@ import { getSupabase } from '../../utils/supabase'
 import { PostgrestError } from '@supabase/supabase-js';
 
 type Data = {
-  name: string
+  message: string, 
+  data: {flow: string, mood: string, craving: string, symptom: string, user_id: string}
 }
 
 export default async function addEntry(
   req: NextApiRequest,
-  res: NextApiResponse<Data | PostgrestError>
+  res: NextApiResponse<Data | PostgrestError | Error>
 ) {
-  const { userID, name, birthday, zodiacSign } = req.body
-  const supabase = getSupabase(userID)
-  const { data, error } = await supabase.from("users").insert({ name: name, birth_date: birthday, zodiac_sign: zodiacSign })
-  
-  if (error) return res.status(400).json(error)
+  const { flow, mood, craving, symptom, date, userID } = req.body;
+  const supabase = getSupabase(userID);
 
-  res.status(200).json(data)
+  try {
+    const { data, error } = await supabase
+      .from("entries")
+      .insert({
+        date,
+        flow,
+        mood,
+        craving,
+        symptom,
+        user_id: userID,
+      })
+      .eq("user_id", userID)
+      .single();
+
+    if (error) {
+      return res.status(400).json(error);
+    }
+
+    res.status(200).json({
+      message: `Entry successfully added for ${date}`,
+      data: data
+    });
+  } catch (error) {
+    res.status(500).json(new Error('Internal Server Error'));
+  }
 }
