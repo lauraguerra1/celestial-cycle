546dc9c40de3ca899370d79ac0d287974729010d
Amber Shipley <ashipley2@me.com>
2023-11-17 20:35:08 -0700

    Add link to logo & create RenderDashboard component

--- src/components/CelestialLogo.tsx
+++ src/components/CelestialLogo.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logoImg from '../../public/images/logo.PNG';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const CelestialLogo = () => {
   return (
-    <Image className='ml-5' width={300} height={100} alt="Logo" src={logoImg} />
+    <Link href="/">
+      <Image className='ml-5' width={300} height={100} alt="Logo" src={logoImg} />
+    </Link>
   );
 }
 
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -11,8 +11,6 @@ import Link from 'next/link';
 
 export default function Dashboard({ isAuthorized, data }: AuthProps) {
   const [user, setUser] = useState<UserData | null>(null);
-  const [error, setError] = useState<boolean>(false);
-  const [userInsights, setUserInsights] = useState<Horoscope>();
   const [loading, setLoading] = useState(true);
   const [date] = useState<Date>(new Date());
   const router = useRouter();
@@ -27,41 +25,45 @@ export default function Dashboard({ isAuthorized, data }: AuthProps) {
     } 
   }, [isAuthorized, data, router]);
 
-  useEffect(() => {
-    if (user) {
-      getHoroscope(getTodaysDate(new Date()), user?.zodiac_sign as string)
-      .then(data => {
-        setUserInsights(data[0]);
-      })
-      .catch(err => {
-        setError(true);
-        console.error(err);
-      })
-    }
-  },[user]);
+  return (
+    <div className='relative h-full flex flex-col fade-in'>
+      {loading ? <LoadingGif /> : <RenderDashboard user={user} date={date}/>}
+      <Navbar />
+    </div>
+  );
+}
+//add prop types
+// on insight page, button is too low to add data
+function RenderDashboard({user, date}: any) {
+  const [error, setError] = useState<boolean>(false);
+  const [userInsights, setUserInsights] = useState<Horoscope>();
+  const router = useRouter();
 
-  const renderDashboard = () => {
+  useEffect(() => {
+  if (user) {
+    getHoroscope(getTodaysDate(new Date()), user?.zodiac_sign)
+    .then(data => {
+      setUserInsights(data[0]);
+    })
+    .catch(err => {
+      setError(true);
+      console.error(err);
+    })
+  }
+},[user]);
+
     return (
-      <div className='mt-10 h-full'>
+      <div className='pt-10 h-full'>
       <CelestialLogo />
       <h1 className='mt-7 text-center text-3xl'>Daily Horoscope for {user ? user.name : ''}</h1>
       <h2 className='text-center text-lg'>{getTodaysDate(new Date())}</h2>
-      <div className='flex justify-center items-center flex-col mb-28'>
+      <div className='flex justify-center items-center flex-col mb-20'>
         <Image width={250} height={100} style={{ width: '60%', height: 'auto', maxWidth: '300px' }} alt="Logo" src={`/images/${user ? user.zodiac_sign : 'capricorn'}.png`} priority/>
         <div className='w-3/4 h-fit lg:w-1/2 mt-2 border border-white border-1 overflow-scroll rounded-lg px-5 py-1'>
-          <p className='md:text-xl'>{error ? "Error loading horoscope, please refresh page" : 
-            !loading? userInsights?.description : <LoadingGif />}</p>
+          <p className='md:text-xl'>{error ? "Error loading horoscope, please refresh page" : userInsights?.description}</p>
         </div>
         <Link href={`${router.asPath.includes('demo') ? '/demo' : ''}/insights/${`${date.getMonth()+1}-${date.getDate()}-${date.getFullYear()}`}`}><button className='bg-grayblue w-60 p-3 m-5 rounded-xl'>View Today&#39;s Insights</button></Link>
       </div>
     </div>
     )
   }
-
-  return (
-    <div className='relative h-full flex flex-col fade-in'>
-      {loading ? <LoadingGif /> : renderDashboard()}
-      <Navbar />
-    </div>
-  );
-}
