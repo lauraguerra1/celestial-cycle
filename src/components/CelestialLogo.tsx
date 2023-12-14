165e83a5a9271ce7ff48b6db1570736dd62594f1
Amber Shipley <ashipley2@me.com>
2023-12-13 20:48:06 -0800

    Add background to logo when page has scrolling

--- src/components/CelestialLogo.tsx
+++ src/components/CelestialLogo.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logoImg from '../../public/images/logo.PNG';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const CelestialLogo = () => {
+  const [scrolling, setScrolling] = useState<Boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolling(window.scrollY > 0);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <Link href="/">
+    <Link href="/" className={`fixed top-0 left-0 z-10 w-full transition-opacity ${scrolling ? 'bg-deepblue' : ''}`}>
       <Image className='ml-5' width={300} height={100} alt="Logo" src={logoImg} />
     </Link>
   );
