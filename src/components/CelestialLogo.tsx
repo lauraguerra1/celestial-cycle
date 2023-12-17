d4e6b863b249d6f698935a3f80334c7f0b5deeda
Amber Shipley <ashipley2@me.com>
2023-12-17 13:03:49 -0800

    Fix bug on logo Link width

--- src/components/CelestialLogo.tsx
+++ src/components/CelestialLogo.tsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import logoImg from '../../public/images/logo.PNG';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const CelestialLogo = () => {
   const [scrolling, setScrolling] = useState<Boolean>(false);
+  const router = useRouter();
+  const { asPath } = router;
 
   useEffect(() => {
     const handleScroll = () => {
@@ -17,9 +20,11 @@ const CelestialLogo = () => {
   }, []);
 
   return (
-    <Link href="/" className={`fixed top-0 left-0 z-10 w-full transition-opacity ${scrolling ? 'bg-deepblue' : ''}`}>
-      <Image className='ml-5' width={300} height={100} alt="Logo" src={logoImg} />
-    </Link>
+    <div className={`fixed top-0 left-0 z-10 transition-opacity max-sm:w-full ${scrolling ? 'bg-deepblue' : ''}`}>
+      <Link href={router.asPath.includes('demo') ? '/demo/dashboard' : '/dashboard'}>
+        <Image className='ml-5' width={300} height={100} alt="Logo" src={logoImg} />
+      </Link>
+    </div>
   );
 }
 
